4aa1fdd4d75e6f07b32b16090f98dc5f6afc3719
kadorek <haltaskadir@gmail.com>
2024-05-16 11:41:14 +0300

    assign role to user view added

--- Fihrist/Areas/Management/Controllers/HomeController.cs
+++ Fihrist/Areas/Management/Controllers/HomeController.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fihrist.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Fihrist.Areas.Management.Controllers
 {
     [Area("Management")]
     public class HomeController : Controller
     {
+        private readonly Fihrist2Context context;
+        public HomeController()
+        {
+            context=new Fihrist2Context();
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult AssignRoleToUser() {
+
+            var members = context.Members.ToList();//db de bulunan tüm kullanıcılar alındı
+            var roles= context.Roles.ToList(); //db bulunan tüm roller alındı
+
+            var selectListMember = new SelectList(members, "Id", "Username");// select elementinde kullanılabilmesi için member listesi düzenlendi
+            var selectListRoles = new SelectList(roles, "Id", "Name");// select elementinde kullanılabilmesi için rol listesi düzenlendi
+
+            ViewBag.ListMember=selectListMember; //viewbag aracıyla memeber listesi view e gönderildi
+            ViewBag.ListRoles=selectListRoles; //viewbag aracıyla role listesi view e gönderildi
+
+
+            return View();        
+        }
+
+
     }
 }
