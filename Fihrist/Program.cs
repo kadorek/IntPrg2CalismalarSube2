8cad3adac7a950e41f3121d1fbdd748f1f9a9475
kadorek <haltaskadir@gmail.com>
2024-05-23 10:09:31 +0300

    route düzeltildi

--- Fihrist/Program.cs
+++ Fihrist/Program.cs
@@ -31,14 +31,20 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
       name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}",
+      defaults: new { area=""}
     );
+
+    endpoints.MapDefaultControllerRoute();
+
 });
+
 app.Run();
